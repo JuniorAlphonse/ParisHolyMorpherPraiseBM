e696a5058a115d2c29d0a29179738e1b35cac68f
Junior Alphonse <97910760+JuniorAlphonse@users.noreply.github.com>
2025-04-17 18:51:21 +0000

    Rewrite the Legend Waddle program and include psuedocode notation along with the code.

---
+++ LegendWaddle.py
@@ -0,0 +1,1 @@
+# The purpose of this program is to explicate the usage of calculial (a type of math used to redefine superposition and rename it as quantumposition, along with tangentially corrected superbits. Superbits are qubits that rely on domain-specific definitions of the partial derivatives of the derivative of the space-time and time-space functional equations.)
