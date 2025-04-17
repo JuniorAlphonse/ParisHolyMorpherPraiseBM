03f2089a2a2525c7abe7ae4e8fe57dbe5dd50c65
Junior Alphonse <97910760+JuniorAlphonse@users.noreply.github.com>
2025-04-17 19:51:40 +0000

    Complete Charmed to Fly proof for non-Pythagorean superposition proof

--- LegendWaddle.py
+++ LegendWaddle.py
@@ -6,13 +6,14 @@ import cmath
 ## dP/dt = 2*y
 
 e = -1;
-e**4 = 1;
-y = 0.5;
-t = [0, 1, 0.5];
+
+## Given the above definition of e, e**4 would be equal to 1, since (-1)**4 = 1.
+
+y = 0.5; 
+multidimensional_time = 0.5
 population_partial_derivative_with_respect_to_time = 2 * y;
-tangential_y_function_with_respect_to_time = t*ln(e**t);
+tangential_y_function_with_respect_to_time = multidimensional_time *math.log((e**multidimensional_time));
+### The integral of dt/dy is 2 seconds displaced
 tangentail_time_with_respect_to_location = 2
 
-### The integral of dt/dy is 2 seconds displaced
-
-[0, 1, 0.5];
+#### Please note that this program is to serve as a proof and not code to be executed. It proves given the above, if our definition of superpositon were defined as x**2 + y**3 + z**4 + n*N*(hcrossbar)*D, then the previous expression would yield all superpositions. With that information calculating safe, and or healthy, superpositions would be possible, relatively easy, meaningful, and manageable.
