96677e7e3033b3a0208b99765c445ad279c4cd0a
Junior Alphonse <97910760+JuniorAlphonse@users.noreply.github.com>
2025-04-17 19:27:36 +0000

    Remodeled Partial Differential Code

--- LegendWaddle.py
+++ LegendWaddle.py
@@ -1,1 +1,18 @@
 # The purpose of this program is to explicate the usage of calculial (a type of math used to redefine superposition and rename it as quantumposition, along with tangentially corrected superbits. Superbits are qubits that rely on domain-specific definitions of the partial derivatives of the derivative of the space-time and time-space functional equations.)
+
+import math
+import cmath
+
+## dP/dt = 2*y
+
+e = -1;
+e**4 = 1;
+y = 0.5;
+t = [0, 1, 0.5];
+population_partial_derivative_with_respect_to_time = 2 * y;
+tangential_y_function_with_respect_to_time = t*ln(e**t);
+tangentail_time_with_respect_to_location = 2
+
+### The integral of dt/dy is 2 seconds displaced
+
+[0, 1, 0.5];
