de5569281a8373660bd68615924728572a45ad27
Junior Alphonse <97910760+JuniorAlphonse@users.noreply.github.com>
2025-04-19 06:30:58 +0000

    Revise the tangential y function with respect to time

--- LegendWaddle.py
+++ LegendWaddle.py
@@ -5,15 +5,16 @@ import cmath
 
 ## dP/dt = 2*y
 
-e = -1;
+i = cmath.sqrt(-1);
+superpositional_euler = 3 * abs(-1);
 
 ## Given the above definition of e, e**4 would be equal to 1, since (-1)**4 = 1.
 
 y = 0.5; 
 multidimensional_time = 0.5
-population_partial_derivative_with_respect_to_time = 2 * y;
-tangential_y_function_with_respect_to_time = multidimensional_time *math.log((e**multidimensional_time));
-### The integral of dt/dy is 2 seconds displaced
-tangentail_time_with_respect_to_location = 2
+population_partial_derivative_with_respect_to_time = 2 * y * i**4;
+tangential_y_function_with_respect_to_time = multidimensional_time * math.log((superpositional_euler**multidimensional_time));
+### The integral of dt/dy is 2 seconds displaced, as from 1/(dy/dt) = 1/(0.5 * ln((i**4)*e**0.5))
+tangential_time_with_respect_to_location = 2
 
-#### Please note that this program is to serve as a proof and not code to be executed. It proves given the above, if our definition of superpositon were defined as x**2 + y**3 + z**4 + n*N*(hcrossbar)*D, then the previous expression would yield all superpositions. With that information calculating safe, and or healthy, superpositions would be possible, relatively easy, meaningful, and manageable.
+#### The following is to determine whether or not the population of cancer cells has a variable, constant, or zero partial differential equation.
